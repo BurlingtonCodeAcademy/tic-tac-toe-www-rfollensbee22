4c9feaf0015108e957ee8e8326291a05fb32485c
Ryan Follensbee <rfollensbee22@gmail.com>
2019-03-18 20:50:41 -0400

    updated main.js and index.html
    Ugly, but this will fulfill the display Player X's portion of the first User Story. This will get refactored.

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+
+playerTurn = "x";
+
 var startButton = document.getElementById("start");
 startButton.addEventListener("click", disableButton);
+startButton.addEventListener("click", playGame);
+
+function playGame() {
+    if (playerTurn = "x") {
+        document.getElementById("playerx").style.display = "initial";
+    }
+}
+
+/*if (playerTurn = "x") {
+    document.getElementById("playerx").style.display = "initial";
+}*/
 
 function disableButton() {
     this.disabled = true;
